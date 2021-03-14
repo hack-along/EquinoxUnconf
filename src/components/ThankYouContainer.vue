af9f1ada9ccb53d910bed4c2e4342703f1333d43
Maija <hello@maijagrudule.com>
2021-03-14 14:07:56 +0100

    Added column split to thank you list

--- src/components/ThankYouContainer.vue
+++ src/components/ThankYouContainer.vue
@@ -2,7 +2,10 @@
   <div class="my-10">
     <h2>Thank you</h2>
     <ul class="pl-1">
-      <li v-for="name in thankyou" :key="name">{{name}}</li>
+      <li
+        v-for="name in thankyou"
+        :key="name"
+      >{{name}}</li>
     </ul>
   </div>
 </template>
@@ -46,4 +49,14 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+ul {
+  -moz-column-count: 4;
+  -moz-column-gap: 20px;
+  -webkit-column-count: 4;
+  -webkit-column-gap: 20px;
+  column-count: 4;
+  column-gap: 20px;
+}
+</style>
 
