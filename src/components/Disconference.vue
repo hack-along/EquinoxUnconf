ed3ccfcefddaf88bcbf07593e72571af9c0c44a9
Roberto Valenti <robbyvalenti@gmail.com>
2021-03-20 12:14:28 +0100

    Updated room css

--- src/components/Disconference.vue
+++ src/components/Disconference.vue
@@ -5,10 +5,10 @@
       <span class="subtitle">{{ subtitle }}</span>
     </div>
     <ul class="pl-1">
-      <li v-for="(room,index) in rooms" :key="index">
+      <li class="room" v-for="(room,index) in rooms" :key="index">
         <h3>{{ discorddata[room].name }} <button @click="openDiscord(discorddata[room].invite)">Join</button> </h3>
         
-          <span
+          <span class="float"
             v-for="(participant,index) in discorddata[room].participants"
             :key="index"
           >
@@ -18,7 +18,7 @@
               class="avatar"
             />
           </span>
-      
+         
       </li>
     </ul>
   </div>
@@ -85,10 +85,21 @@ export default {
 
 <style scoped>
 
+.float{
+  float:left
+}
+
 .avatar {
   vertical-align: middle;
   width: 50px;
   height: 50px;
   border-radius: 50%;
+}
+
+.room {
+  padding: 5px 5px 70px 5px;
+  margin: 5px;
+  border-radius: 5%;
+  border:solid;
 }
 </style>
